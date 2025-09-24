d7d08077480d10e19024d72044daeee7c3cc23e0
iwawaq <poshchadite@gmail.com>
2025-09-24 20:19:40 +0700

    Update

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,2 +1,3 @@
 read time_str
 date '+%H час/а/ов %M минут/ы' -d "$time_str"
+unset time_str
--- solution/task3.sh
+++ solution/task3.sh
@@ -2,3 +2,5 @@ read date1
 export LC_TIME=ru_RU.UTF-8
 date2=$(date -d "$date1" '+%d %B %Y года, %A')
 echo "$date2"
+unset date2
+unset date1
