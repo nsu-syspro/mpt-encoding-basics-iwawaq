911313de0a1f3e1635478c1e2721bc8280740b15
iwawaq <poshchadite@gmail.com>
2025-09-24 19:50:02 +0700

    Update

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,4 +1,3 @@
-cd solution/
-enca -L ru -x UTF-8 task2.sh
-enca -L ru -x UTF-8 task3.sh
-enca -L ru -x UTF-8 task4.sh
+iconv -f KOI8-R -t UTF-8 task2.sh -o temp.txt && mv temp.txt task2.sh
+iconv -f IBM866 -t UTF-8 task3.sh -o temp.txt && mv temp.txt task3.sh
+iconv -f ISO-8859-5 -t UTF-8 task4.sh -o temp.txt && mv temp.txt task4.sh
